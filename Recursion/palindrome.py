390d6c589ae5c3623547827dd6b643a062b2eec2
Ibraheem101 <46232711+Ibraheem101@users.noreply.github.com>
2022-08-19 21:16:35 +0100

    Update palindrome.py

--- Recursion/palindrome.py
+++ Recursion/palindrome.py
@@ -1,1 +1,9 @@
+def join(x):
+    return "".join(x.lower().split())
 
+def is_palindrome(x):
+    # Check if x[0] == x[-1]
+    if (len(x) == 1) or (len(x) == 2 and x[0] == x[-1]):
+        return True
+    if join(x[0]) == join(x[-1]):
+        return is_palindrome(join(x[1:-1]))
